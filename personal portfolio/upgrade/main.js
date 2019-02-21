cc7b98406a6dcaf03cef8bc5ca298a0622aa1057
Camille <theothertotalbookworm@gmail.com>
2019-02-21 11:20:36 -0700

    imported films info and set it up to pull each of the seven

--- personal portfolio/upgrade/main.js
+++ personal portfolio/upgrade/main.js
@@ -1,7 +1,17 @@
 import { films } from './films.js'
 
-let titleElement = document.querySelector('.title')
-let crawlElement = document.querySelector('.crawl')
+const intro = document.querySelector('.intro')
 
-titleElement.textContent = films[0].title
-crawlElement.textContent = films[0].opening_crawl
+films.forEach( (film) => {
+    console.log(film.title)
+
+let titleElement = document.createElement('h1')
+intro.appendChild(titleElement)
+titleElement.textContent = film.title
+
+let crawlElement = document.createElement('h3')
+intro.appendChild(crawlElement)
+crawlElement.textContent = film.opening_crawl
+
+let lineBreak = document.createElement('hr')
+})
