c0f938603d0fa9a115395d52744f32f82d98632f
Camille <theothertotalbookworm@gmail.com>
2019-02-28 11:13:23 -0700

    made some changes added modules

--- personal portfolio/upgrade/main.js
+++ personal portfolio/upgrade/main.js
@@ -1,4 +1,5 @@
 import { films } from './films.js'
+import { people } from './people.js'
 
 const intro = document.querySelector('.intro')
 
@@ -8,14 +9,20 @@ films.forEach( (film) => {
 let tile = document.createElement('div')
 
 let titleElement = document.createElement('h1')
-intro.appendChild(titleElement)
+tile.appendChild(titleElement)
 titleElement.textContent = film.title
 
 let crawlElement = document.createElement('h3')
-intro.appendChild(crawlElement)
+tile.appendChild(crawlElement)
 crawlElement.textContent = film.opening_crawl
 
 let lineBreak = document.createElement('hr')
 })
 
-intro.appendChild(tile)
+
+
+const female = people.filter(person => person.gender === "female");
+const male = people.filter(person => person.gender === "male");
+const other = people.filter(person => person.gender === "n/a");
+const none = people.filter(person => person.gender === "none");
+const hermaphrodite = people.filter(person => person.gender === "hermaphrodite");
