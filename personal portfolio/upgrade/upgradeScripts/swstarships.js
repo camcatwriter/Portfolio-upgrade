28610b11ddc7e1e8b0b0d3bbc4bca31d7aee5117
Camille <theothertotalbookworm@gmail.com>
2019-03-15 16:57:42 -0600

    starship unknown sorts properly

--- personal portfolio/upgrade/upgradeScripts/swstarships.js
+++ personal portfolio/upgrade/upgradeScripts/swstarships.js
@@ -3,8 +3,25 @@ import { starships } from './starships.js'
 const intro = document.querySelector('.intro')
 
 
+starships.sort(function (a,b) {
+    console.log(a.length,b.length)
+    if (a.length == "unknown") {
+        console.log("a statement")
+        return -1;
+    }
+    if (b.length == "unknown") {
+        console.log("b statement")
+        return 1;
+    }
+    if (parseFloat(a.length, 10) > parseFloat(b.length, 10)) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+} )
 
-starships.sort((a,b) => (parseFloat(a.length, 10) > parseFloat(b.length, 10)) ? 1 : -1 )
+// starships.sort((a,b) => (parseFloat(a.length, 10) > parseFloat(b.length, 10)) ? 1 : -1 )
 
 // function sortStarships(a, b) {
 //     return a.length > b.length ? -1 : b.length > a.length ? 1 : 0;
