6c77444008428a08605fc80ab79e77d6103d1744
Camille <theothertotalbookworm@gmail.com>
2019-03-14 20:33:28 -0600

    base code on both

--- personal portfolio/upgrade/upgradeScripts/swstarships.js
+++ personal portfolio/upgrade/upgradeScripts/swstarships.js
@@ -1,2 +1,19 @@
 import { starships } from '.../data lists/starships.js'
 
+const intro = document.querySelector('.intro')
+
+films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
+
+films.forEach(film => {
+let tile = document.createElement('div')
+
+let titleElement = document.createElement('h2')
+tile.appendChild(titleElement)
+titleElement.textContent = film.title
+
+let crawlElement = document.createElement('div')
+tile.appendChild(crawlElement)
+crawlElement.textContent = film.opening_crawl
+
+intro.appendChild(tile)
+})
