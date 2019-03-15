5d934afa1f8083dee66ef52191fcafea9ab5aec7
Camille <theothertotalbookworm@gmail.com>
2019-03-15 14:39:10 -0600

    PLANETS SHOW UP

--- personal portfolio/upgrade/upgradeScripts/swplanets.js
+++ personal portfolio/upgrade/upgradeScripts/swplanets.js
@@ -1,24 +1,28 @@
-// import { planets } from '../upgradeScripts/planets.js'
+console.log("Hello world");
+// import { films } from '../data lists/films.js'
+import { planets } from './planets.js'
 
-// const intro = document.querySelector('.intro')
+const intro = document.querySelector('.intro')
 
 
 
 
-// planets.sort((a,b) => (a.diameter > b.diameter) ? 1 : -1 )
+planets.sort((a,b) => (parseInt(a.diameter) > parseInt(b.diameter)) ? 1 : -1 )
 
-// planets.forEach(planet => {
-// let tile = document.createElement('div')
+planets.forEach(planet => {
+let tile = document.createElement('div')
 
-// let name = document.createElement('h2')
-// tile.appendChild(name)
-// name.textContent = planet.name
+let name = document.createElement('h2')
+tile.appendChild(name)
+name.textContent = planet.name
 
-// console.log("names coming through now");
+console.log(planet.name);
 
 // let terrain = document.createElement('div')
 // tile.appendChild(terrain)
 // terrain.textContent = planet.terrain
 
-// })
+intro.appendChild(tile)
+})
+
 
