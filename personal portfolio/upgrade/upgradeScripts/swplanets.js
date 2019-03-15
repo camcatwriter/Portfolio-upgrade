06855132db4546556a4322e357ac3d8fa3edfb85
Camille <theothertotalbookworm@gmail.com>
2019-03-14 23:26:50 -0600

    nothing works

--- personal portfolio/upgrade/upgradeScripts/swplanets.js
+++ personal portfolio/upgrade/upgradeScripts/swplanets.js
@@ -1,20 +1,24 @@
-import { planets } from 'planets.js'
+// import { planets } from '../upgradeScripts/planets.js'
 
-const intro = document.querySelector('.intro')
+// const intro = document.querySelector('.intro')
 
-planets.sort((a,b) => (a.diameter > b.diameter) ? 1 : -1 )
 
-planets.forEach(planet => {
-let tile = document.createElement('div')
 
-let name = document.createElement('h2')
-tile.appendChild(name)
-name.textContent = planet.name
 
-console.log("names coming through now");
+// planets.sort((a,b) => (a.diameter > b.diameter) ? 1 : -1 )
 
-let terrain = document.createElement('div')
-tile.appendChild(terrain)
-terrain.textContent = planet.terrain
+// planets.forEach(planet => {
+// let tile = document.createElement('div')
 
-})
+// let name = document.createElement('h2')
+// tile.appendChild(name)
+// name.textContent = planet.name
+
+// console.log("names coming through now");
+
+// let terrain = document.createElement('div')
+// tile.appendChild(terrain)
+// terrain.textContent = planet.terrain
+
+// })
+
